00461fdf3caf8be0c14bbf4f958895e7b064a2a1
nnethercott <natenethercott@gmail.com>
2025-02-11 20:01:25 +0100

    mini

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -10,13 +10,22 @@ return {
         view = {style = 'sign',},
       })
       require("mini.comment").setup()
-      require("mini.trailspace").setup()
+
+      local trailspace = require("mini.trailspace")
+      trailspace.setup({
+        vim.keymap.set("n", "<leader>cw", function() trailspace.trim() end)
+      })
+
       require("mini.splitjoin").setup()
       require("mini.surround").setup({
         mappings = {
           add = 'e',
           delete = 'ds',
           replace = 'cs',
+          find = 'ef',
+          find_left = 'eF',
+          highlight = 'eh',
+          update_n_lines = 'en',
         }
       })
     end,
