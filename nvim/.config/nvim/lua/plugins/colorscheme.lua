568514dcd9676c3cb2362ff4ba0115171fd32619
Nate Nethercott <nathaniel.nethercott@mistral.ai>
2026-01-07 08:53:11 +0100

    more gruvbox

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -19,6 +19,7 @@ return {
           "zenbones",
           "zenwritten",
           "gruvbox",
+          "gruvbox-material",
           "fluoromachine",
         },
       })
@@ -124,6 +125,15 @@ return {
         strings = false,
       },
     },
+  },
+  {
+    "sainnhe/gruvbox-material",
+    config = function ()
+      vim.g.gruvbox_material_background = "medium"
+      vim.g.gruvbox_material_foreground = "original"
+      vim.g.gruvbox_material_enable_italic = 0
+      vim.g.gruvbox_material_enable_bold = 1
+    end
   },
   {
     "maxmx03/fluoromachine.nvim",
