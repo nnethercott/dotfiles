c5f9c5297bf37d1c01bee77b11440a3a83a5d172
Nate Nethercott <naten@Nates-MacBook-Pro.local>
2026-02-02 22:03:31 +0100

    tmux

--- nvim/.config/nvim/lua/plugins/experimental.lua
+++ nvim/.config/nvim/lua/plugins/experimental.lua
@@ -71,11 +71,4 @@ return {
       },
     },
   },
-  {
-  "Maxteabag/sqlit.nvim",
-  opts = {},
-  keys = {
-    { "<leader>D", function() require("sqlit").open() end, desc = "Database (sqlit)" },
-  },
-}
 }
