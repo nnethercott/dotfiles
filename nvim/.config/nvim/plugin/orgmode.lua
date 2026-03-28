3d7a4cc2b20e738e64606b873fa9314965e680ce
nate <natenethercott@gmail.com>
2026-03-28 07:44:18 +0100

    orgmode

---
+++ nvim/.config/nvim/plugin/orgmode.lua
@@ -0,0 +1,42 @@
+vim.pack.add({
+	"https://github.com/nvim-orgmode/orgmode",
+	"https://github.com/chipsenkbeil/org-roam.nvim",
+})
+
+require("orgmode").setup({
+	org_agenda_files = "~/orgfiles/**/*",
+	org_default_notes_file = "~/orgfiles/refile.org",
+	org_startup_folded = "showeverything",
+	mappings = {
+		capture = {
+			org_capture_refile = "R",
+			org_capture_finalize = "q",
+		},
+	},
+})
+require("org-roam").setup({
+	directory = "~/orgfiles",
+	org_files = {
+		"~/orgfiles/**/*.org",
+	},
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "org",
+	callback = function()
+		vim.keymap.set("n", "<C-]>", function()
+			require("orgmode").action("org_mappings.open_at_point")
+		end, { buffer = true })
+	end,
+})
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "org-roam-select",
+	callback = function()
+		vim.b.completion = false
+		vim.keymap.set("i", "<C-j>", "<C-n>", { buffer = true, remap = true })
+		vim.keymap.set("i", "<C-k>", "<C-p>", { buffer = true, remap = true })
+	end,
+})
+
+vim.keymap.set("n", "<leader>nc", require("org-roam.api").capture_node, { desc = "Roam: capture node" })
+vim.keymap.set("n", "<leader>nf", require("org-roam.api").find_node, { desc = "Roam: find node" })
